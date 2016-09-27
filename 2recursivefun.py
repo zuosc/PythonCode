8a78d91464da015579454a147cb19781bbc15309
zuosc <zorro.zsc@hotmail.com>
2016-09-27 23:38:44 +0800

    调整

--- 2recursivefun.py
+++ 2recursivefun.py
@@ -2,9 +2,11 @@
 # Power by zuosc 2016-09-27
 # 递归函数
 
+
 def fact(n):
     if n == 1:
         return 1
     return n * fact(n - 1)
+
 
 fact(7)
---
+++ 4slice.py
@@ -0,0 +1,7 @@
+# -*- coding:utf8 -*-
+# Power by zuosc 2016-09-27
+# 数据切片
+
+L=[‘A’，‘B’，‘C’，‘D’，‘E’，‘F’，‘G’]
+
+print(L[:3])
