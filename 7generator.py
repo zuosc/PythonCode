d6123a11014869f4265599daa8f6e3a126e232e5
zuosc <zorro.zsc@hotmail.com>
2016-10-08 20:23:23 +0800

    生成器

--- 7generator.py
+++ 7generator.py
@@ -3,4 +3,18 @@
 #generator 生成器 (一边循环一边计算的机制，称为生成器：generator)
 
 g = (x * x for x in range(10))
-for 
+for n in g:
+    print(n)
+print('---------------')
+
+
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        print(b)
+        a, b = b, a + b
+        n = n + 1
+    return 'done'
+
+
+fib(10)
