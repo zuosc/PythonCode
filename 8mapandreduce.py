bc575081a6ad570f03753bca11e1f4b9727e3706
zuoscccc <zorro.zsc@hotmail.com>
2016-10-12 18:06:17 +0800

    reduce

--- 8mapandreduce.py
+++ 8mapandreduce.py
@@ -36,3 +36,56 @@ def fn(x, y):
 
 r = reduce(fn, [1, 2, 3, 9])
 print(r)
+print('---------------------------')
+
+from functools import reduce
+
+
+def fn(x, y):
+    return x * 10 + y
+
+
+def char3num(s):
+    return {'0': 0,
+            '1': 1,
+            '2': 2,
+            '3': 3,
+            '4': 4,
+            '5': 5,
+            '6': 6,
+            '7': 7,
+            '8': 8,
+            '9': 9}[s]
+
+
+r = reduce(fn, map(char3num, '44885'))
+
+print(r)
+
+#################################################################
+
+from functools import reduce
+
+
+def str2int(s):
+    def fn(x, y):
+        return x * 10 + y
+
+    def char2num(s):
+        return {'0': 0,
+                '1': 1,
+                '2': 2,
+                '3': 3,
+                '4': 4,
+                '5': 5,
+                '6': 6,
+                '7': 7,
+                '8': 8,
+                '9': 9}[s]
+
+#使用lambda
+#return reduce(lambda x, y: x * 10 + y, map(char2num, s))
+
+    return reduce(fn, map(char2num, s))
+
+print(str2int('123123123123'))
