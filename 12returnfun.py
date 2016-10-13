ad3b95527fa133609706522a6a163ed22b90f779
zuoscccc <zorro.zsc@hotmail.com>
2016-10-13 16:17:56 +0800

    闭包

---
+++ 12returnfun.py
@@ -0,0 +1,48 @@
+# -*- coding:utf8 -*-
+# Power by zuosc
+# 返回函数
+
+
+def calc_sum(*args):
+    ax = 0
+    for n in args:
+        ax = ax + n
+    return ax
+
+
+def lazy_sum(*args):
+    def sum():
+        ax = 0
+        for n in args:
+            ax = ax + n
+        return ax
+
+    return sum
+
+
+f = lazy_sum(1, 3, 5, 7, 9)
+print(f)
+print(f())
+
+print('-------------------------')
+f1 = lazy_sum(1, 3, 5, 7, 9)
+f2 = lazy_sum(1, 3, 5, 7, 9)
+print(f1 == f2)
+
+# 闭包
+# 返回闭包时牢记的一点就是：返回函数不要引用任何循环变量，或者后续会发生变化的变量。
+
+def count():
+    fs = []
+    for i in range(1, 4):
+
+        def f():
+            return i * i
+
+        fs.append(f)
+    return fs
+
+
+f1, f2, f3 = count()
+
+print(f1())
