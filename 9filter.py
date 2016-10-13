f90425e6c817ee4ca80f5c4080af4b8f6e0eca3c
zuoscccc <zorro.zsc@hotmail.com>
2016-10-13 11:07:55 +0800

    素数的生成

--- 9filter.py
+++ 9filter.py
@@ -13,3 +13,31 @@ def not_empty(s):
 
 
 print(list(filter(not_empty, ['a', 'bb', None, 'cccc'])))
+
+# 素数的获取
+
+
+def _odd_iter():
+    n = 1
+    while True:
+        n = n + 2
+        yield n
+
+
+def _not_divisible(n):
+    return lambda x: x % n > 0
+
+
+def primes():
+    yield 2
+    it = _odd_iter()  # 初始序列
+    while True:
+        n = next(it)  # 返回序列的第一个数字
+        yield n
+        it = filter(_not_divisible(n), it)
+
+for n in primes():
+    if n < 1000:
+        print(n)
+    else:
+        break
