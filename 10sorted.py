a31a50e3db8568437530d7e1d284224c71c33cee
zuoscccc <zorro.zsc@hotmail.com>
2016-10-13 14:35:57 +0800

    排序

--- 10sorted.py
+++ 10sorted.py
@@ -2,3 +2,9 @@
 # Power by zuosc 2016-10-13
 # 排序算法
 
+from operator import itemgetter
+students = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
+
+print(sorted(students, key=itemgetter(0)))
+print(sorted(students, key=lambda t: t[1]))
+print(sorted(students, key=itemgetter(1), reverse=True))
